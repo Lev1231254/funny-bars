eec40459ca912a091a89e0d67b40038462c73329
Leva <novikovlev48@gmail.com>
2025-07-21 18:35:45 +0300

    change window to RenderWindow

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,34 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
 
+    sf::CircleShape greenCircle(50.f);
+    greenCircle.setFillColor(sf::Color(100, 250, 50));
+
+    sf::RectangleShape redRectangle({120, 50});
+    redRectangle.setFillColor(sf::Color::Red);
+    redRectangle.setPosition({300, 300});
+
+
+    
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        window.draw(greenCircle);
+        window.draw(redRectangle);
+
         window.display();
     }
-}
+}           
